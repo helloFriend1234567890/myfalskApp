39e9b8850926a0449ab7c630ba864651bce4dcac
helloFriend1234567890 <yashsandipaher3621@gmail.com>
2024-10-12 20:46:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import pickle
 import numpy as np
+import os
 
 app = Flask(__name__)
 
@@ -22,4 +23,5 @@ def predict():
     return jsonify(predictions)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Get port from environment variable
+    app.run(host='0.0.0.0', port=port, debug=True)
